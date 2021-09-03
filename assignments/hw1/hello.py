2c8964e0cdc27b58b604e4a5255f416c1e6829e1
J-R3MY <millerjd1@g.cofc.edu>
2021-09-03 09:53:27 -0400

    completed hw1

---
+++ assignments/hw1/hello.py
@@ -0,0 +1,14 @@
+"""
+Name: Jeremy Miller
+hello.py
+
+Problem: This program prints "hello, world!"
+
+Certificate of Authenticity:
+I certify that this assignment is entirely my own work
+"""
+
+def main():
+    print("hello, world!")
+
+main()
