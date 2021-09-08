cab58e789cd0a5e5aadb78336990ae5de243d6e5
J-R3MY <millerjd1@g.cofc.edu>
2021-09-08 15:17:10 -0400

    completed lab2

--- labs/lab2/lab2.py
+++ labs/lab2/lab2.py
@@ -1,4 +1,55 @@
 """
-Name: <your name goes here – first and last>
-<ProgramName>.py
+Name: Jeremy Miller
+lab2.py
+
+Problem: This program performs arithmetic
 """
+import math
+
+def sum_of_threes():
+    bound = eval(input("Enter a number: "))
+    acc = 0
+    for i in range(0, bound+1, 3):
+        acc = acc+i
+
+    print(acc)
+
+
+def muliplication_table():
+    for h in range(1, 11):
+        for l in range(1, 11):
+            print(h*l, end=" ")
+        print()
+
+def triangle_area():
+    a = eval(input("Input the length for side a: "))
+    b = eval(input("Input the length for side b: "))
+    c = eval(input("Input the length for side c: "))
+    s = (a + b + c)/2
+    A = (s*(s-a)*(s-b)*(s-c))
+    Area = math.sqrt(A)
+    print(Area)
+
+def sumSquares():
+    start = eval(input("Enter the start number: "))
+    end = eval(input("Enter the ending number: "))
+    acc = 0
+    for i in range(start, end+1):
+        acc = acc + i**2
+    print(acc)
+
+
+def power():
+    base = eval(input("Enter a base number: "))
+    exp = eval(input("Enter an exponent: "))
+    acc = 1
+    for i in range(exp):
+        acc = acc * base
+
+    print(base, "^", exp, "=", acc)
+
+sum_of_threes()
+muliplication_table()
+triangle_area()
+sumSquares()
+power()
